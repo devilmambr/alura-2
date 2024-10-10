8fe1d05bec9297b853719b62f4ea4279e114aa6a
devilmambr <154016885+devilmambr@users.noreply.github.com>
2024-10-10 09:35:12 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'qual cantor favorito ',
+    '|Luan Santana'
+)
+
+criaCartao(
+    'pessoal',
+    'qual minha cor favorita?',
+    'Azul'
+)
+
+criaCartao(
+    'Curiosidade',
+    'melhor jogador do mundo?',
+    'Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Curiosidade',
+    'uma viagem dos sonhos?',
+    'Egito'
+)
